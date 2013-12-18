13dafe9196142f7a51aa9ba3d9afafd74d7b7cd9
kaingeo <kaingeo@yahoo.gr>
2013-12-18 17:15:05 +0200

    1

--- js/mean.js
+++ js/mean.js
@@ -3,23 +3,15 @@
  * 
  */
 
-$(function(){
-	
-	 function init (){
-		$("body").append('<div id="container"></div>');
-    	$("#container").append('<div id="structure"></div>'); 
-    	$("#container").append('<div id="main"></div>');
-    	$("#container").append('<div id="visual"></div>');
-     };
-	
-    
-   	$("p").click(function(){
-   		$(this).fadeOut();
-   		alert("You clicked");
-	});
-	
-	
-	
-	
-});
 
+function init (){
+	alert("test");
+	$("body").append('<div id="container"></div>');
+  	$("#container").append('<div id="structure"></div>'); 
+  	$("#container").append('<div id="main"></div>');
+  	$("#container").append('<div id="visual"></div>');
+};
+    
+
+
+	
