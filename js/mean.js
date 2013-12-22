40355924d15e99e7260ae9e606bdb0d8a2a6f8b8
kaingeo <kaingeo@yahoo.gr>
2013-12-22 15:06:05 +0200

    1

--- js/mean.js
+++ js/mean.js
@@ -1,17 +1,40 @@
 /**
  * @author kaingeo
  * 
+ * 
+ * 
+ * 
+ * function init (){
+		alert("kalaeisaii");
+	
+		$("body").append('<div id="container"></div>');
+	   	$("#container").append('<div id="structure"></div>'); 
+	   	$("#container").append('<div id="main"></div>');
+   		$("#container").append('<div id="visual"></div>');
+};
+
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  */
 
+jQuery.fn.extend({
+    init : function () {
+    	//alert("kala");
+    	//$(class).append('<div id="container"></div>');
+	   	//$("#container").append('<div id="structure"></div>'); 
+	   	//$("#container").append('<div id="main"></div>');
+   		//$("#container").append('<div id="visual"></div>');
+    }
+});
 
-function init (){
-	$("body").append('<div id="container"></div>');
-   	$("#container").append('<div id="structure"></div>'); 
-   	$("#container").append('<div id="main"></div>');
-   	$("#container").append('<div id="visual"></div>');
-};
+
 	
-	
+
 	
 	
 		
